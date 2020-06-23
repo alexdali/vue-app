72e31efd87f7c09aa851c93e921f74d95ce54a1d
alexdali <alexdali@gmail.com>
2020-06-23 18:58:44 +0600

    add views and routes. Add config for Firebase DB

---
+++ src/firebaseConfig.js
@@ -0,0 +1,58 @@
+//import firebase from "firebase";
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+// firebase init goes here
+const config = {
+  apiKey: "AIzaSyCuEAWniV6tQJiyQUFZaf_2nLgY1EbZqpc",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "vue-app-5095b",
+  storageBucket: "",
+  messagingSenderId: ""
+};
+firebase.initializeApp(config);
+
+// firebase utils
+const db = firebase.firestore();
+const auth = firebase.auth();
+const currentUser = auth.currentUser;
+
+// date issue fix according to firebase
+// const settings = {
+//   timestampsInSnapshots: true
+// };
+// db.settings(settings);
+db.settings({});
+
+// firebase collections
+const usersCollection = db.collection("users");
+const postsCollection = db.collection("posts");
+const commentsCollection = db.collection("comments");
+const likesCollection = db.collection("likes");
+
+export {
+  db,
+  auth,
+  currentUser,
+  usersCollection,
+  postsCollection,
+  commentsCollection,
+  likesCollection
+};
+
+// It looks like you're using the development build of the Firebase JS SDK.
+// When deploying Firebase apps to production, it is advisable to only import
+// the individual SDK components you intend to use.
+
+// For the module builds, these are available in the following manner
+// (replace <PACKAGE> with the name of a component - i.e. auth, database, etc):
+
+// CommonJS Modules:
+// const firebase = require('firebase/app');
+// require('firebase/<PACKAGE>');
+
+// ES Modules:
+// import firebase from 'firebase/app';
+// import 'firebase/<PACKAGE>';
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,37 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Login from "@/views/Login";
+import Dashboard from "@/views/Dashboard";
+import Settings from "@/views/Settings";
+import { auth } from "../firebaseConfig";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home
+    path: "*",
+    redirect: "/dashboard"
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/login",
+    name: "Login",
+    component: Login
+  },
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    component: Dashboard,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: "/settings",
+    name: "Settings",
+    component: Settings,
+    meta: {
+      requiresAuth: true
+    }
   }
 ];
 
@@ -25,6 +39,21 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes
+});
+
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some(x => x.meta.requiresAuth);
+  const currentUser = auth.currentUser;
+
+  //   if (requiresAuth && !currentUser) {
+  //     next("/login");
+  //   } else if (requiresAuth && currentUser) {
+  //     next();
+  //   } else next();
+  // });
+  requiresAuth && !currentUser && next("/login");
+  requiresAuth && currentUser && next();
+  next();
 });
 
 export default router;
