a12443635542feec5da19c9e342865d286d2f764
alexdali <alexdali@gmail.com>
2020-06-24 18:48:20 +0600

    update store: add func for handle page reload

--- src/store/index.js
+++ src/store/index.js
@@ -1,30 +1,38 @@
-import Vue from "vue";
-import Vuex from "vuex";
-const fb = require("../firebaseConfig");
+import Vue from 'vue';
+import Vuex from 'vuex';
+const fb = require('../firebaseConfig');
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
+//handle page reload
+fb.auth.onAuthStateChanged(user => {
+  if (user) {
+    store.commit('setCurrentUser', user);
+    store.dispatch('fetchUserProfile');
+  }
+});
+
+const store = new Vuex.Store({
   state: {
     currentUser: null,
-    userProfile: {}
+    userProfile: {},
   },
   actions: {
     clearData({ commit }) {
-      commit("setCurrentUser", null);
-      commit("setUserProfile", {});
+      commit('setCurrentUser', null);
+      commit('setUserProfile', {});
     },
     fetchUserProfile({ commit, state }) {
       fb.usersCollection
         .doc(state.currentUser.uid)
         .get()
         .then(res => {
-          commit("setUserProfile", res.data());
+          commit('setUserProfile', res.data());
         })
         .catch(err => {
-          console.log("actions fetchUserProfile error:", err);
+          console.log('actions fetchUserProfile error:', err);
         });
-    }
+    },
   },
   mutations: {
     setCurrentUser(state, val) {
@@ -32,7 +40,9 @@ export default new Vuex.Store({
     },
     setUserProfile(state, val) {
       state.userProfile = val;
-    }
+    },
   },
-  modules: {}
+  modules: {},
 });
+
+export default store;
