75b645cf8dbfc7764c12cfd18f62e561333a9a00
alexdali <alexdali@gmail.com>
2020-06-24 10:06:36 +0600

    update eslintrc, Login view

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,14 @@ module.exports = {
   env: {
     node: true
   },
+  "plugins": ["prettier"],
   // extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
-  extends: ["plugin:vue/essential", "@vue/prettier"],
+  extends: ["prettier", "plugin:prettier/recommended", "plugin:vue/essential", "@vue/prettier"],
   parserOptions: {
     parser: "babel-eslint"
   },
   rules: {
+    //"prettier/prettier": "error",
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
   }
---
+++ .prettierrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  //trailingComma: "es5", default
+  tabWidth: 4,
+  //semi: true, default
+  singleQuote: false,
+};
